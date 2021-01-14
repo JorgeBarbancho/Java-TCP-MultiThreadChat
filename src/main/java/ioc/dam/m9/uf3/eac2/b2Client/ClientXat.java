5b55f5fefa6cd7b618f0b7746985e6912621c9fc
Jorge Barbancho <jbarbancho@gmail.com>
2021-01-14 15:19:32 +0100

    First commit

---
+++ src/main/java/ioc/dam/m9/uf3/eac2/b2Client/ClientXat.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ioc.dam.m9.uf3.eac2.b2Client;
+
+/**
+ *
+ * 
+ */
+public class ClientXat {
+    
+    public static void main(String[] args) {
+        
+        FinestraPrincipal finestra = new FinestraPrincipal();
+        finestra.rebreMissatgesServidor();
+    }    
+}
